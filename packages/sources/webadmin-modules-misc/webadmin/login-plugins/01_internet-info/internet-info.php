4da21a29b266b585667b3e2f6379e2fccda5937a
MissKittin <you@example.com>
2021-04-16 15:20:42 +0000

    webadmin js vulnerability patch

--- packages/sources/webadmin-modules-misc/webadmin/login-plugins/01_internet-info/internet-info.php
+++ packages/sources/webadmin-modules-misc/webadmin/login-plugins/01_internet-info/internet-info.php
@@ -1,6 +1,6 @@
 <?php if(!function_exists('prevent_direct')) include $system['location_php'] . '/lib/prevent-direct.php'; prevent_direct('internet-info.php'); ?>
 <div>
-	<?php echo shell_exec($system['location_php'] . '/login-plugins/' . $plugin . '/shell.sh check-internet'); // IPv4 ?>
+	<?php echo str_replace('&lt;br&gt;', '<br>', htmlspecialchars(shell_exec($system['location_php'] . '/login-plugins/' . $plugin . '/shell.sh check-internet'))); ?>
 	<br>
-	<?php echo shell_exec($system['location_php'] . '/login-plugins/' . $plugin . '/shell.sh check-ipv6'); // IPv6 ?>
+	<?php echo str_replace('&lt;br&gt;', '<br>', htmlspecialchars(shell_exec($system['location_php'] . '/login-plugins/' . $plugin . '/shell.sh check-ipv6'))); ?>
 </div>
